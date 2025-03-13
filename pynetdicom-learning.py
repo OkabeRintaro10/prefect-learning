c9fc99ddd07e3f19f44e214c98340904aa7037bf
Ryefoxlime <kausikremella@gmail.com>
2025-03-13 07:33:51 +0000

    Basic ae connection to check connection and echo scp

---
+++ pynetdicom-learning.py
@@ -0,0 +1,17 @@
+from pynetdicom import AE
+
+ae = AE()
+ae.add_requested_context("1.2.840.10008.1.1")
+assoc = ae.associate("127.0.0.1", 11112)
+if assoc.is_established:
+    print("Association established with Echo SCP!")
+    status = assoc.send_c_echo()
+    if status:
+        print("C-ECHO Request accepted")
+    else:
+        print("Connection timed out, was aborted or received invalid response")
+
+    assoc.release()
+else:
+    # Association rejected, aborted or never connected
+    print("Failed to associate")
